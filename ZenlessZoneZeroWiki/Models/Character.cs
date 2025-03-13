f4a1877f7199c9b9a1c3aa383fee70916c5eedae
Jo Art Mahilaga <Joartmahilaga69@gmail.com>
2025-03-13 15:02:26 +0000

    Added `ZenlessZoneZeroContext` class in `ZenlessZoneZeroContext.cs` to define the database context for Entity Framework Core, including `DbSet` properties for `User`, `Character`, `Weapon`, and `Favourites` entities. Configured a composite key for the `Favourites` entity.
    Added `Character` class in `Character.cs` to represent characters with properties for `CharacterID`, `HP`, `AttackDMG`, `Defence`, `Element`, and `CharacterType`, along with a collection of `Favourites`.
    
    Added `Favourites` class in `Favourites.cs` to represent the relationship between users, characters, and weapons, including properties for `UserID`, `CharacterID`, and `WeaponID`, and navigation properties to `User`, `Character`, and `Weapon`.
    
    Added `User` class in `User.cs` to represent users with properties for `UserID`, `Username`, `Email`, `FirstName`, `LastName`, `TimeCreated`, and a collection of `Favourites`.
    
    Added `Weapon` class in `Weapons.cs` to represent weapons with properties for `WeaponID`, `Name`, `Damage`, `WeaponType`, and a collection of `Favourites`.

---
+++ ZenlessZoneZeroWiki/Models/Character.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ZenlessZoneZeroWiki.Models
+{
+    public enum Element
+    {
+        Fire, Water, Earth, Air, Electric
+    }
+
+    public enum CharacterType
+    {
+        Attacker, Defender, Support
+    }
+
+    public class Character
+    {
+        [Key]
+        public int CharacterID { get; set; }
+
+        [Required]
+        public int HP { get; set; }
+
+        [Required]
+        public int AttackDMG { get; set; }
+
+        [Required]
+        public int Defence { get; set; }
+
+        [Required]
+        public Element Element { get; set; }
+
+        [Required]
+        public CharacterType Type { get; set; }
+
+        public ICollection<Favourites> Favourites { get; set; }
+    }
+
+}
---
+++ ZenlessZoneZeroWiki/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ZenlessZoneZeroWiki.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        [Required, StringLength(100)]
+        public string Username { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, StringLength(100)]
+        public string FirstName { get; set; }
+
+        [Required, StringLength(100)]
+        public string LastName { get; set; }
+
+        public DateTime TimeCreated { get; set; } = DateTime.UtcNow;
+
+        public ICollection<Favourites> Favourites { get; set; }
+    }
+}
