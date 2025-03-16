9ad1010ef220c80833514510134eed17d7e922b2
aloysius <aloysiuspacheco2003@gmail.com>
2025-03-16 16:36:52 +0000

    Add properties to Character and update enums
    - Added `Name`, `Description`, and `faction` properties to the `Character` class in `Character.cs`.
    - Updated `WeaponType` and `Element` enums in `Enums.cs` to use uppercase naming conventions and introduced a new `Faction` enum with several factions.
    - Modified routing configuration in `Program.cs` for improved syntax consistency.

--- ZenlessZoneZeroWiki/Models/Character.cs
+++ ZenlessZoneZeroWiki/Models/Character.cs
@@ -7,7 +7,9 @@ namespace ZenlessZoneZeroWiki.Models
     {
         [Key]
         public int CharacterID { get; set; }
-
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Enum faction { get; set; }
         public int HP { get; set; }
         public int Attack { get; set; }
         public int Defence { get; set; }
