f43a05c506b63438bc26518b9d794f5527339d90
Jo Art Mahilaga <Joartmahilaga69@gmail.com>
2025-03-17 04:49:29 +0000

    new stuff part 6

--- ZenlessZoneZeroWiki/Program.cs
+++ ZenlessZoneZeroWiki/Program.cs
@@ -1,30 +1,37 @@
-using Microsoft.EntityFrameworkCore;
-using ZenlessZoneZeroWiki.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ZenlessZoneZeroContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+// 1. Register services
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
 
 var app = builder.Build();
 
+// 3. Use environment-specific middleware
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
 
+// 4. Configure the HTTP pipeline
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
 app.UseAuthorization();
 
+
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}"
-);
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+app.MapBlazorHub();
 
 app.Run();
